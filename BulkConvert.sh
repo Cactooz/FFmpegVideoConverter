bb6901f2e07e710b81415e798ab1a90485cee0fa
Cactooz <54435874+Cactooz@users.noreply.github.com>
2021-04-29 19:22:26 +0200

    Convert all the found files

--- BulkConvert.sh
+++ BulkConvert.sh
@@ -6,3 +6,9 @@ OUTPUTFILEEND=$4
 
 shopt -s nullglob
 FILES=($DIRECTORY/*.$FILEEND)
+
+for FILE in $FILES; do
+  FILENAME=${FILE%.*}
+  
+  ffmpeg -i "$DIRECTORY/$FILE" -c copy -c:s mov_text -loglevel warning "$DIRECTORY/$FILENAME.$OUTPUTFILEEND"
+done
